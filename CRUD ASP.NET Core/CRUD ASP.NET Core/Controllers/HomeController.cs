7693a357740fd54255c32724adf72c3030b9c5f7
Enzo Milanese <enzo12ms@gmail.com>
2019-09-24 10:05:22 -0300

    Adicionado controller principal

---
+++ CRUD ASP.NET Core/CRUD ASP.NET Core/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CRUD_ASP.NET_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_ASP.NET_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IContatoRepository _contatoRepository;
+
+        public HomeController(IContatoRepository contatoRepository)
+        {
+            _contatoRepository = contatoRepository;
+        }
+
+        public IActionResult Index()
+        {
+            Contato model = _contatoRepository.GetContato(1);
+            return View(model);
+        }
+    }
+}
